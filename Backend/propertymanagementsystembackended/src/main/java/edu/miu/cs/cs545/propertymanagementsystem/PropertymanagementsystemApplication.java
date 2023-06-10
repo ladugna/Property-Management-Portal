0dbbe7522178a33a09e455d2c52be0c75541805a
LEMESSA ADUGNA <abdibori52@gmail.com>
2023-06-10 09:59:45 -0500

    first push

---
+++ Backend/propertymanagementsystembackended/src/main/java/edu/miu/cs/cs545/propertymanagementsystem/PropertymanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs545.propertymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertymanagementsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertymanagementsystemApplication.class, args);
+    }
+
+}
---
+++ Backend/propertymanagementsystembackended/src/test/java/edu/miu/cs/cs545/propertymanagementsystem/PropertymanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs545.propertymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertymanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
