670ad1e947dbfe643f0f24a7feba3b539cdc3704
tekeste teklehaimanot <tmehark@gmail.com>
2023-06-20 04:27:08 -0500

    a

--- FrontEnd/property-management-system-frontended/src/Components/PageNotFound.js
+++ FrontEnd/property-management-system-frontended/src/Components/PageNotFound.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Box, Button, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
-
 const primary = "rgba(0,117,223,0.5)"
 
 export default function() {
@@ -25,9 +24,11 @@ export default function() {
       <Typography variant="h1" style={{ color: 'white' }}>
         404
       </Typography>
+
       <Typography variant="h6" style={{ color: 'white' }}>
         The page you’re looking for doesn’t exist.
       </Typography>
+      
       <Button variant="contained" onClick={goBack}>Back Home</Button>
     </Box>
   );
